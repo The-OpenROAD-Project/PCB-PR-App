63a4767036887e73e951c48652f68acf821bf93a
Chester Holtz <chholtz@moore.ucsd.edu>
2019-11-18 07:36:01 -0800

    move .py files into lib

--- create_shared_objs.sh
+++ create_shared_objs.sh
@@ -1,8 +1,16 @@
- #!/bin/bash 
+#!/bin/bash 
 PCBPLACERPATH=./module/SA-PCB/
 PCBROUTERPATH=./module/PcbRouter/
 KICADDBPATH=./module/KicadParser/
 
-LIBDIR=./lib
+LIBDIR=./lib/
 
-gcc -shared ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/GridBasedPlacer.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/globalParam.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/readFiles.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/PcbPlacer_wrapper.cpp.o ${KICADDBPATH}lib/libkicadpcbparser.a -o ${LIBDIR}src/_PcbPlacer.so -lstdc++
+gcc -shared ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/GridBasedPlacer.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/globalParam.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/readFiles.cpp.o ${PCBPLACERPATH}bin/CMakeFiles/PCBPLACERlib.dir/src/PcbPlacer_wrapper.cpp.o ${KICADDBPATH}lib/libkicadpcbparser.a -o ${LIBDIR}_PcbPlacer.so -lstdc++
+
+gcc -shared ${PCBROUTERPATH}bin/CMakeFiles/pcbrouterlib.dir/src/BoardGrid.cpp.o ${PCBROUTERPATH}bin/CMakeFiles/pcbrouterlib.dir/src/GridBasedRouter.cpp.o ${PCBROUTERPATH}bin/CMakeFiles/pcbrouterlib.dir/src/globalParam.cpp.o ${PCBROUTERPATH}bin/CMakeFiles/pcbrouterlib.dir/src/PcbRouter_wrapper.cpp.o ${KICADDBPATH}lib/libkicadpcbparser.a -o ${LIBDIR}_PcbRouter.so -lstdc++
+
+gcc -shared ${KICADDBPATH}bin/CMakeFiles/kicadpcbparserlib.dir/src/kicadPcbDataBase.cpp.o ${KICADDBPATH}bin/CMakeFiles/kicadpcbparserlib.dir/src/kicadParser.cpp.o ${KICADDBPATH}bin/CMakeFiles/kicadpcbparserlib.dir/src/shape.cpp.o ${KICADDBPATH}bin/CMakeFiles/kicadpcbparserlib.dir/src/util.cpp.o ${KICADDBPATH}bin/CMakeFiles/kicadpcbparserlib.dir/src/PcbDB_wrapper.cpp.o ${KICADDBPATH}lib/libkicadpcbparser.a -o ${LIBDIR}_PcbDB.so -lstdc++
+
+cp ${PCBPLACERPATH}src/PcbPlacer.py ./lib
+cp ${PCBROUTERPATH}src/PcbRouter.py ./lib
+cp ${KICADDBPATH}src/PcbDB.py ./lib
