116975a2292532a4ae2e73a4d610edb8d3b36af0
Chester Holtz <chholtz@moore.ucsd.edu>
2019-12-18 14:27:03 -0800

    demo

--- examples/test_flow.py
+++ examples/test_flow.py
@@ -1,10 +1,10 @@
 from ucsdpcb import PcbPlacer, PcbRouter, PcbDB
 
-db = PcbDB.kicadPcbDataBase('../module/PCBBenchmarks/bm9/bm9.routed.kicad_pcb')
+db = PcbDB.kicadPcbDataBase('./module/PCBBenchmarks/bm9/bm9.routed.kicad_pcb')
 db.printNodes()
 placer = PcbPlacer.GridBasedPlacer(db)
 placer.set_inner_iter(1)
-placer.set_outer_iter(2)
+placer.set_outer_iter(1)
 placer.test_placer_flow()
 db.printNodes()
 router = PcbRouter.GridBasedRouter(db)
