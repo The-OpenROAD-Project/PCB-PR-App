b4dfeae9d7e340e9277aeb4baedfb711a879f388
Chester Holtz <chholtz@moore.ucsd.edu>
2020-01-04 09:55:50 -0800

    typo in prop script tree traversal

--- util/check_output.py
+++ util/check_output.py
@@ -1,34 +1,37 @@
 from nltk import Tree
-def postprocess_layer(t, cur_layer=""):
-    if len(t) == 0 or isinstance(t, str):
-        return
-
-    # if current label is module, find the layer it's on
-    if t.label() == "module":
-        for c in t:
-            if isinstance(c, str):
-                continue
-            elif c.label() == "layer":
-                cur_layer = c[0]
-    elif t.label == "pad": # if pad, set the layer to cur layer
-        for c in t:
-            if isinstance(c, str):
-                continue
-            elif c.label() == "layers":
-                # iterate through layers and swap out layer type for the cur_layer
-                for cc in c:
-                    if cc == "Top" or cc == "Bottom":
-                        cc.set_label(cur_layer)
-    for c in t:
-        postprocess_layer(c, cur_layer)
 
 def check_output(fname):
+    def postprocess_layer(t, cur_layer=""):
+        if len(t) == 0 or isinstance(t, str):
+            return
+
+        # if current label is module, find the layer it's on
+        if t.label() == "module":
+            for c in t:
+                if isinstance(c, str):
+                    continue
+                elif c.label() == "layer":
+                    cur_layer = c[0]
+        elif t.label() == "pad": # if pad, set the layer to cur layer
+            for c in t:
+                if isinstance(c, str):
+                    continue
+                elif c.label() == "layers":
+                    # iterate through layers and swap out layer type for the cur_layer
+                    for i,cc in enumerate(c):
+                        if cc == "Top" or cc == "Bottom":
+                            c[i] = cur_layer
+        for c in t:
+            postprocess_layer(c, cur_layer)
+
     # read file into tree structure
+    print('reading ' + fname)
     s = open(fname, 'r').read().replace('\n','')
     t = Tree.fromstring(s)
 
     postprocess_layer(t)
 
+    print('writing ' + './checked.'+fname,)
     # write tree to file
-    with open('./'+fname+'_parsed_output','w') as f:
+    with open('./checked.'+fname,'w') as f:
         f.write(str(t))
