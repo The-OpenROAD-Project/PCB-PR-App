0a7943ca7d59bc51ee3a9d53423e25da4768332f
Devon <9127315+djmerrill@users.noreply.github.com>
2020-01-08 12:33:03 -0800

    Update ubuntu_install.sh

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -1,1 +1,12 @@
+# Tested on Ubuntu 18.04
 
+export CXX=g++
+sudo apt-get update
+sudo apt-get install swig
+sudo apt-get install libboost-all-dev
+sudo apt-get install cmake
+git clone --recurse-submodules https://github.com/The-OpenROAD-Project/PCB-PR-App.git
+cd PCB-PR-App
+source clean.sh
+source build.sh
+cd ..
