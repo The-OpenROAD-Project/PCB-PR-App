1da49ca48cb1c873c4b70abb582abac259f6cbfe
Devon <djmerrill@ucsd.edu>
2020-05-14 18:55:54 +0000

    line ending problem

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,2 +1,1 @@
-##docker build -t pcb-pr-app .
-docker run -v $(pwd):/PCB-PR-App ubuntu:18.04 bash -c "/PCB-PR-App/jenkins/install.sh"
+docker run -v $(pwd):/PCB-PR-App ubuntu:18.04 bash -c "./PCB-PR-App/jenkins/install.sh"
