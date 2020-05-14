b162cc292d4e74a1f46e5a14613d2ac07618403f
Devon <9127315+djmerrill@users.noreply.github.com>
2020-05-14 11:00:19 -0700

    removed sudos

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -2,11 +2,11 @@
 # git clone --recurse-submodules https://github.com/The-OpenROAD-Project/PCB-PR-App.git
 
 export CXX=g++
-sudo apt-get update
-sudo apt-get install -y swig
-sudo apt-get install -y libboost-all-dev
-sudo apt-get install -y cmake
-sudo apt install -y python3-pip
+apt-get update
+apt-get install -y swig
+apt-get install -y libboost-all-dev
+apt-get install -y cmake
+apt install -y python3-pip
 pip3 install docopt
 source clean.sh
 source build.sh
