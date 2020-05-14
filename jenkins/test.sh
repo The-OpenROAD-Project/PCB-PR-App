acbe9e1ffe6d336762eb65cf974a297d68c638e1
Devon <djmerrill@ucsd.edu>
2020-05-14 21:28:19 +0000

    added a test

---
+++ jenkins/test.sh
@@ -0,0 +1,1 @@
+docker run -v $(pwd):/PCB-PR-App ubuntu:18.04 bash -c "./PCB-PR-App/tests/regression-py.sh"
---
+++ tests/regression-py.sh
@@ -0,0 +1,9 @@
+#set -e
+
+echo "Running tests .."
+echo "================"
+
+echo "run_layout.py test"
+python3 run_layout.py
+echo "Success"
+
