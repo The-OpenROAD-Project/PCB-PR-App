a2c945f4dbb16620519f4f9e4c94ddc893caca1b
Devon <9127315+djmerrill@users.noreply.github.com>
2020-05-14 17:50:04 -0700

    Update build.sh

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,3 +1,3 @@
-git submodule update --init --recursive
+#git submodule update --init --recursive # This is now done with a Jenkins config setting
 docker build -t pcbprapp .
 docker run -v $(pwd):/PCB-PR-App pcbprapp bash -c "./PCB-PR-App/jenkins/install.sh"
